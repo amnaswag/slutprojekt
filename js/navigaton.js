d2ad32cb22341123e69a493ae857ac41f9ac23d7
amna <94680997+amnaswag@users.noreply.github.com>
2022-05-16 21:18:08 +0200

    Update navigaton.js

--- js/navigaton.js
+++ js/navigaton.js
@@ -2,14 +2,18 @@ function myFunction(x) {
   x.classList.toggle("change");
 }
 
+
+// get user selected text
 const getUserSelectedText = () => {
   return window.getSelection().toString();
 }
 
+// knowing when user reaches the end of the screen 
 window.onscroll = () => {
   if(window.innerHeight + window.pageY0ffset) >= document.body.offsetHeight) {
     // console.log("Där slutade sidan.") 
   }
 }
 
-const toTop = () = window.scrollTo({top: 0, behaviour: 'smooth'})
+// taking user back to top
+const toTop = () => window.scrollTo({top: 0, behaviour: 'smooth'}):
