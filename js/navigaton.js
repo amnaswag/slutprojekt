98b1f80f3ea24c98d7f3e614818f4994b009f242
amna <94680997+amnaswag@users.noreply.github.com>
2022-05-22 17:50:57 +0200

    Add files via upload

--- js/navigaton.js
+++ js/navigaton.js
@@ -1,19 +1,41 @@
-function myFunction(x) {
-  x.classList.toggle("change");
+
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
 }
 
-
-// get user selected text
-const getUserSelectedText = () => {
-  return window.getSelection().toString();
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
 }
 
-// knowing when user reaches the end of the screen 
-window.onscroll = () => {
-  if(window.innerHeight + window.pageY0ffset) >= document.body.offsetHeight) {
-    // console.log("Där slutade sidan.") 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
-// taking user back to top
-const toTop = () => window.scrollTo({top: 0, behaviour: 'smooth'}):
